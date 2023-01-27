a30b70d89dc4b4acde0a43a48d108cf852c43cb5
Irina Chapurina <chapurina-85@yandex.ru>
2023-01-27 21:09:33 +0300

    Init commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,16 @@
+public class Post {
+    public String name; // поле для имени
+    // добавьте другие поля
+    public String patronymic;
+    public String surname;
+    public String passport;
+    public String phone;
+    public boolean subscription;
+
+
+    FormDate birthday = new FormDate();
+
+
+
+}
+
